eb63694d790e59544a164ad9b0b0cca042eab38a
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-04-08 13:00:34 +0100

    Initial commit

---
+++ skoteangular-430/Skote_Angular_v4.3.0/Admin/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  defaultauth: 'firebase',
+  firebaseConfig: {
+    apiKey: "AIzaSyBmOWtjPSNUAYKkxsDhmIQC0tmJcY_rr_I",
+    authDomain: "assurance-41299.firebaseapp.com",
+    projectId: "assurance-41299",
+    storageBucket: "assurance-41299.appspot.com",
+    messagingSenderId: "456252211861",
+    appId: "1:456252211861:web:a1d67dd3c8029db265919c",
+    measurementId: "G-T72FH3NL7V"
+  }
+};
